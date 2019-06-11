00105e3317cf78b0d63eeb607525827e036b01cf
Justin Hutsell <hutselljustin@gmail.com>
2019-06-11 14:40:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,36 @@
-class Artist 
-  attr_accessor :name
+# class Artist 
+#   attr_accessor :name
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name)
-    @name = name 
-    @songs = []
+#   def initialize(name)
+#     @name = name 
+#     @songs = []
     
-    @@all << self
-  end
+#     @@all << self
+#   end
   
-  def add_song(song)
-    @songs << song
-    # song.artist = self
-  end
+#   def add_song(song)
+#     @songs << song
+#     # song.artist = self
+#   end
   
-  def save 
-    @@all << self 
-  end
+#   def save 
+#     @@all << self 
+#   end
   
-  def self.find_or_create_by_name(name)
-    found = @@all.find { |artist| artist.name == name }
-    if found
-      found
-    else
-      self.new(name)
-    end
-  end
+#   def self.find_or_create_by_name(name)
+#     found = @@all.find { |artist| artist.name == name }
+#     if found
+#       found
+#     else
+#       self.new(name)
+#     end
+#   end
   
-  def print_songs
-    @songs.each { |song| puts song.name }
-  end
+#   def print_songs
+#     @songs.each { |song| puts song.name }
+#   end
   
   
-end
+# end
