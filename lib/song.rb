d24c3b7678bf224561aef1943aa1fff0579fd7e8
Justin Hutsell <hutselljustin@gmail.com>
2019-06-11 14:41:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,3 @@
-# class Song 
-#   attr_accessor :name, :artist, :song
-  
-#   def initialize(name)
-#     @name = name 
-#   end
-  
-#   def self.new_by_filename(file_name)
-#     arr = file_name.split(" - ")
-#     new_song = self.new(arr[1])
-#     new_song.artist = (arr[0])
-#     new_song
-#   end
-# end
-
 class Song
   attr_accessor :name, :artist
 
