df467ff68db3b17a4a81b903bca3320f76e3f6a2
Justin Hutsell <hutselljustin@gmail.com>
2019-06-11 14:29:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(file_name)
     arr=file_name.split(" - ")
     new_song=self.new(arr[1])
-    new_song.artist_name=(arr[0])
+    new_song.artist=(arr[0])
     new_song
   end
   
